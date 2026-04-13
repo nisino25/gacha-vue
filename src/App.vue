50af6f0f9640b2134f0ddd62c4437a23699ef8be
Nozomu Ando <nisino25@gmail.com>
2026-04-14 00:00:39 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <span v-for="(item, index) in gachadata" :key="index">
+    {{ item.name }}
+  </span>
+</template>
+
+<script>
+import data from "./const/data.json"
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      gachadata: data,
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
